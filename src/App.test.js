e7b06e041cb01609c70dc1d0443db91aa27fed48
Pete Seeling <peteseeling@gmail.com>
2022-01-28 13:01:35 -0600

    test fixed

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('BJ 40', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/BJ 40/i);
   expect(linkElement).toBeInTheDocument();
 });
